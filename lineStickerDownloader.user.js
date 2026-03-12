0afa9ced46db8dcaed25e08bc515740a01f26246
Tina Zhao <tinayzhao@gmail.com>
2026-03-11 22:57:32 -0700

    chore(line sticker downloader): - bump patch version - add more commentary context to match readme - add console log to display download url used

--- lineStickerDownloader.user.js
+++ lineStickerDownloader.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Line Sticker Downloader
-// @version      1.0.0
+// @version      1.0.1
 // @author       manacake.co
 // @namespace    manacake.co
 // @description  Downloads the page's stickers in a .zip file
@@ -20,7 +20,7 @@
   const stickerNameContainer = document.querySelector('p[data-test="sticker-name-title"]');
   const isAnimated = !!document.querySelector('div[ref="mainImage"]').querySelector('span[data-test="animation-sticker-icon"]');
   /**
-   * Due to the endpoint not having a valid secure protocol, there is a possibility that the download might not complete properly without manual intervention. Usually, modern browsers will stop the .zip download because the current document is served over https while the download is served over http. In order to follow through, you must click [Allow download] manually by checking your browser's downloads UI.
+   * Due to the endpoint not having a valid secure protocol, there is a possibility that the download might not complete properly without manual intervention. Usually, modern browsers will stop the .zip download because the current document is served over https while the download is served over http. In order to follow through, you must click [Allow download] manually by checking your browser's downloads UI. Also, if the .zip gets corrupted somehow, try downloading in a different browser. (You can peek the download URL in the console)
    */
   const staticTemplateUrl = 'http://dl.stickershop.line.naver.jp/products/0/0/1/[ID]/iphone/stickers@2x.zip';
   const animatedTemplateUrl = 'http://dl.stickershop.line.naver.jp/products/0/0/1/[ID]/iphone/stickerpack@2x.zip';
@@ -43,10 +43,12 @@
   buttonDownload.setAttribute('style', 'display:block; padding:15px; margin-bottom:10px; background:#00B84F; color:#FFF; font-weight:bold; font-size:15px; cursor:pointer;');
   buttonDownload.innerHTML = 'Download all stickers';
   buttonDownload.onclick = () => {
-    window.open(`${isAnimated
+    const downloadUrl = `${isAnimated
       ? downloadEndpointAnimated
       : downloadEndpointStatic
-      }`, '_blank');
+    }`;
+    console.log(`[Line Sticker Downloader] Attempting to download at ${downloadUrl}`);
+    window.open(downloadUrl, '_blank');
   }
   stickerNameContainer.after(buttonDownload);
 })();
